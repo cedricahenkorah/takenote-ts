e4834877084355d1ed75ab1d1abd88f8723ed976
Cedric Ahenkorah <cedricahenkorah@gmail.com>
2023-12-17 22:23:14 +0000

    added the route for the login page

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import HomePage from "../pages/HomePage";
+import AuthLayout from "../pages/auth/AuthLayout";
+import LoginPage from "../pages/auth/LoginPage";
 
 const Routes = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <HomePage />,
+    },
+    {
+      path: "/auth",
+      element: <AuthLayout />,
+      children: [
+        {
+          path: "login",
+          element: <LoginPage />,
+        },
+      ],
     },
   ]);
 
