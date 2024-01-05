2de78e37904a924393c7260f5552bf2638535b5a
cedricahenkorah <cedricahenkorah@gmail.com>
2024-01-05 19:42:54 +0000

    auth context set up

---
+++ src/context/authContext.tsx
@@ -0,0 +1,69 @@
+import {
+  ReactElement,
+  createContext,
+  useEffect,
+  useReducer,
+  Dispatch,
+} from "react";
+
+interface AuthState {
+  user: object | null | undefined;
+}
+
+interface AuthAction {
+  type: string;
+  payload?: object;
+}
+
+interface AuthContextProps {
+  user: AuthState;
+  dispatch: Dispatch<AuthAction>;
+}
+
+const initialState: AuthState = {
+  user: {},
+};
+
+type ChildrenType = { children?: ReactElement | ReactElement[] };
+
+export const AuthContext = createContext<AuthContextProps | undefined>({
+  user: initialState,
+  dispatch: () => {},
+});
+
+export const authReducer = (
+  state: AuthState,
+  action: AuthAction
+): AuthState => {
+  switch (action.type) {
+    case "LOGIN":
+      return { user: action.payload };
+    case "LOGOUT": {
+      return { user: null };
+    }
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider = ({
+  children,
+}: ChildrenType): ReactElement => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user") || "null");
+
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
+
+  console.log("AuthContext state:", state);
+
+  return (
+    <AuthContext.Provider value={{ user: state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
